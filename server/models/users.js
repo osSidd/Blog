c4a55ed6ca1706ef4a1c9a35a8d5362e588e57ce
Osama <107386980+osSidd@users.noreply.github.com>
2024-02-09 22:07:39 +0530

    added simple validation for user schema

--- server/models/users.js
+++ server/models/users.js
@@ -9,22 +9,36 @@ const UserSchema = new Schema({
     name:{
         type:String,
         required:true,
+        minLength:3,
+        validate:{
+            validator: v => /^[A-Za-z\s]+$/.test(v)
+        }
     },
     username:{
         type: String,
-        required: true
+        required: true,
+        minLength: 5,
+        validate:{
+            validator: v => /^[A-Za-z0-9]+$/.test(v)
+        }
     },
     email:{
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        validate:{
+            validator: v => /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/.test(v)
+        }
     },
     password:{
         type:String,
         required:true,
+        minLength:8,
     },
     confirmPassword:{
         type: String,
         required: true,
+        minLength:8,
     },
 })
 
